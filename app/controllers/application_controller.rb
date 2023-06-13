023f7e099244f4e7cbc2b8323c49c25dec68e328
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-13 09:40:29 +0300

    routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,38 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get '/users' do
+    content_type :json
+    User.all.to_json(include: :tasks)
+  end
+
+  get '/tasks' do
+    content_type :json
+    Task.all.to_json(include: :user)
+  end
+  
+  post '/users' do
+    User.create(name: params[:name]).to_json
+    
+  end
+
+  put '/users' do
+    User.create(name: params[:name]).to_json
+    
+  end
+
+  post '/tasks' do
+    Task.create(params[:task])
+    
+  end
+
+  delete '/tasks/:id' do
+    Task.find(params[:id]).destroy
+    redirect '/users'
+  end
+
+  delete '/users/:id' do
+    User.find(params[:id]).destroy.to_json
+  end
+  
 end

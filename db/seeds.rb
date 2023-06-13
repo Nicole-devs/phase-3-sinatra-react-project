8193768039ff33344e8f10e866a3390ea18ea201
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-13 09:42:06 +0300

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
-puts "🌱 Seeding fitness..."
+puts "🌱 Seeding tasks..."
 
 # Seed your database here
+User.delete_all
+User.create(name:'james');
+
+Task.destroy_all
+Task.create(description:'Wash the House', user_id:1)
+Task.create(description:'Launder', user_id:1)
+
 
 puts "✅ Done seeding!"
